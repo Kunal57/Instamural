e4c008bf40c87e52bd4dbe72296b590c3e5d4137
Craig Waterman <email": null>
2016-08-25 17:07:12 -0500

    Created user method wins

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ActiveRecord::Base
   belongs_to  :team, foreign_key: :captian_id
   has_many  :teams, through: :roster
   has_many  :games, through: :teams
+
+  def wins
+    wins = 0
+    self.teams.each do |team|
+      wins += team.wins
+    end
+    wins
+  end
+
 end
