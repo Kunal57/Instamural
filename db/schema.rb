4ab7174981eddf64e398edb3f0cec12b78ffe123
Kunal Patel <kunalpatelx92@gmail.com>
2016-08-25 16:56:08 -0500

    Clean up files

--- db/schema.rb
+++ db/schema.rb
@@ -36,13 +36,7 @@ ActiveRecord::Schema.define(version: 20160825192427) do
   end
 
   create_table "teams", force: :cascade do |t|
-    t.string   "name",        null: false
-    t.integer  "captain_id",  null: false
-    t.integer  "sport_id",    null: false
-    t.string   "mascot_name"
-    t.string   "mascot_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
