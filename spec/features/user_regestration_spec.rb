5a308f64abd7973e1b1e725aed85987648228cb0
Craig Waterman <email": null>
2016-08-25 08:55:36 -0500

    working on rspec new user fill_in

--- spec/features/user_regestration_spec.rb
+++ spec/features/user_regestration_spec.rb
@@ -4,22 +4,22 @@ feature 'create new user' do
   scenario 'goes to new registration page' do
     visit '/'
 
-    click_link('Register')
+    click_link('sign up')
 
-    expect(page).to have_content("Register for new account")
+    expect(page).to have_content("Get ready to play!")
   end
 
   scenario 'create accout' do
     visit '/users/new'
 
-    fill_in 'username', with: 'John'
+    fill_in 'first_name', with: 'John'
 
-    fill_in('email', with: 'Doe')
+    fill_in'email', with: 'john@doe.com'
 
-    fill_in('password', with: 'helloworld')
+    fill_in('password', with: 'password')
 
-    click_link('Register')
+    click_link('submit')
 
-    expect(page).to have_content("Your rad playlist!")
+    expect(page).to have_content("Welcome")
   end
 end
