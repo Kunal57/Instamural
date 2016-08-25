b7fb2f828dc580beafc5679e889c97719116a4e3
Kunal Patel <kunalpatelx92@gmail.com>
2016-08-25 10:53:19 -0500

    Create functionality to edit and delete sports

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -4,18 +4,44 @@ class SportsController < ApplicationController
 	end
 
 	def new
+		@sport = Sport.new
 	end
 
 	def create
 		@sport = Sport.new(post_params)
-		@sport.save
 
-		redirect_to @sport
+		if @sport.save
+			redirect_to @sport
+		else
+			render 'new'
+		end
 	end
 
 	def show
 		@sport = Sport.find(params[:id])
 	end
+
+	def edit
+		@sport = Sport.find(params[:id])
+	end
+
+	def update
+		@sport = Sport.find(params[:id])
+
+		if @sport.update(params[:sport].permit(:name, :max_players, :min_players))
+			redirect_to @sport
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@sport = Sport.find(params[:id])
+		@sport.destroy
+
+		redirect_to sports_path
+	end
+
 
 	private
 
--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,2 +1,6 @@
 class Sport < ActiveRecord::Base
+	validates :name, presence: true
+	validates :max_players, presence: true
+	validates :min_players, presence: true
+	validates :min_players, numericality: { greater_than: 0, message: "must be greater than 0" }
 end
