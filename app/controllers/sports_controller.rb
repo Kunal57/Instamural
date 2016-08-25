f77bc1a75c7a2bee251b6a9b71f5b7dd349c8de0
Kunal Patel <kunalpatelx92@gmail.com>
2016-08-25 10:22:11 -0500

    Create index, new, create, show for sports

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -1,2 +1,26 @@
 class SportsController < ApplicationController
+	def index
+		@sports = Sport.all.order('created_at ASC')
+	end
+
+	def new
+	end
+
+	def create
+		@sport = Sport.new(post_params)
+		@sport.save
+
+		redirect_to @sport
+	end
+
+	def show
+		@sport = Sport.find(params[:id])
+	end
+
+	private
+
+		def post_params
+			params.require(:sports).permit(:name, :max_players, :min_players)
+		end
+
 end
